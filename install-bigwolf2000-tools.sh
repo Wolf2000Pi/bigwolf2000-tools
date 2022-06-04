860abe9707fa30266482e83e562a6be065f6a29b
Wolf2000 <support@wolf2000.at>
2022-06-05 01:51:18 +0200

    Name

--- install-bigwolf2000-tools.sh
+++ install-bigwolf2000-tools.sh
@@ -5,9 +5,9 @@ echo "   \033[36m Autoinstaller für Wolf2000-Tools\033[0m"
 echo
 echo "   \033[36m Author:     Wolf2000\033[0m"
 echo "   \033[36m Version:         1.0\033[0m"
-echo "   \033[36m https://wolf200.at\033[0m"
+echo "   \033[36m https://wolf2000.at\033[0m"
 echo
-echo "   \033[32m Wollen sie Wolf2000-Tools installieren\033[0m"
+echo "   \033[32m Wollen sie Bigwolf2000-Tools installieren\033[0m"
 echo "   \033[32m Ihre Antwort, n/j:\033[0m"
 read answer
 #echo Das installieren wurde abgebrochen
