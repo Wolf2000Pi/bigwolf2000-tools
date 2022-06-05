927922b89490e8cc53aa64cfeffd4e5a57a9b90a
Wolf2000 <support@wolf2000.at>
2022-06-05 03:03:51 +0200

    v1.4

--- bigwolf2000-config.sh
+++ bigwolf2000-config.sh
@@ -325,14 +325,14 @@ while true; do
 done
 
 do_finish() {
-  disable_raspi_config_at_boot
-  if [ $ASK_TO_REBOOT -eq 1 ]; then
+#  disable_raspi_config_at_boot
+ # if [ $ASK_TO_REBOOT -eq 1 ]; then
     whiptail --yesno "Would you like to reboot now?" 20 60 2
-    if [ $? -eq 0 ]; then # yes
-      sync
-      reboot
-    fi
-  fi
+#    if [ $? -eq 0 ]; then # yes
+ #     sync
+ #     reboot
+#    fi
+ # fi
   exit 0
 }
 
