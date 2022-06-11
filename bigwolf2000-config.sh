786e4ad00b7e01f9bfc11ab6700b7c7c55397202
Wolf2000 <support@wolf2000.at>
2022-06-11 19:23:17 +0200

    Update bigwolf2000-config.sh

--- bigwolf2000-config.sh
+++ bigwolf2000-config.sh
@@ -255,11 +255,8 @@ do_docker_purge() {
 }  
 
 do_omv_plugins() {
-echo "deb https://dl.bintray.com/openmediavault-plugin-developers/erasmus jessie main" > /etc/apt/sources.list.d/omv-extras-org.list &&
-sleep 1 &&
-echo "deb https://dl.bintray.com/openmediavault-plugin-developers/erasmus-testing jessie main" | sudo tee -a /etc/apt/sources.list.d/omv-extras-org.list &&
 apt-get update &&
-apt-get --yes --force-yes --allow-unauthenticated install openmediavault-resetperms openmediavault-locate openmediavault-apttool openmediavault-sensors 
+apt-get --yes --force-yes --allow-unauthenticated install openmediavault-resetperms openmediavault-locate openmediavault-apttool  
 exec bigwolf2000-config
 }
 
