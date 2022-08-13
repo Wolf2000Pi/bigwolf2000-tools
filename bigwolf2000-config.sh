432dc01e3122a75cfb71b974a103b68b4088c4e1
Wolf2000 <support@wolf2000.at>
2022-08-13 21:01:17 +0200

    Update bigwolf2000-config.sh

--- bigwolf2000-config.sh
+++ bigwolf2000-config.sh
@@ -219,8 +219,7 @@ do_internationalisation_menu() {
   fi
 }
 do_tasksel() {
-  tasksel &&
-  exit	
+  tasksel 	
 }  
 do_Openmediavault_menu() {
   FUN=$(whiptail --title "Server Software Configuration Tool (Bigwolf2000-config)" --menu "Openmediavault Optionen" $WT_HEIGHT $WT_WIDTH $WT_MENU_HEIGHT --cancel-button Zurrück --ok-button Wählen \
