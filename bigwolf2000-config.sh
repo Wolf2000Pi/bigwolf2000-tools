abc0b7eda2f80bfb912c23df7a042bb02dfd629d
Wolf2000 <support@wolf2000.at>
2022-08-27 23:16:39 +0200

    Update bigwolf2000-config.sh

--- bigwolf2000-config.sh
+++ bigwolf2000-config.sh
@@ -387,9 +387,9 @@ do_deinstall() {
 calc_wt_size
 while true; do
   FUN=$(whiptail --title "Server Software Configuration Tool Bigwolf2000 Version 1.9.5" --menu "Setup Options" $WT_HEIGHT $WT_WIDTH $WT_MENU_HEIGHT --cancel-button Beenden --ok-button Wählen \
-    "1 Docker" "Löschen, usw." \
+    "1 Docker                    " "Löschen, usw." \
 	"2 Change User Password      " "Root Password ändern" \
-    "3 Grund-optionen            " "Sprache-Zeit-Tastatur " \
+    "3 Grund-optionen            " "Sprache-Zeit-Tastatur Tasksel Backup " \
     "4 Erweiterte Optionen       " "Hostname SSH " \
 	"5 System Update             " "Update und upgrade" \
     "6 Openmediavault            " "Installation mit Plugins" \
