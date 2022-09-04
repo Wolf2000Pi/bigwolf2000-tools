d891695a3dbeb9cfd1baed30ab562df54974209c
Wolf2000 <support@wolf2000.at>
2022-09-04 20:24:06 +0200

    Update bigwolf2000-config.sh

--- bigwolf2000-config.sh
+++ bigwolf2000-config.sh
@@ -2,8 +2,9 @@
 # Part of Wolf2000-Tools https://github.com/Wolf2000Pi/bigwolf2000-tools
 # Version 2.0.0
 # by Wolf2000
-
-$vers=cat /root/bigwolf2000-tools/Version
+version() {
+cat /root/bigwolf2000-tools/Version
+}
 
 
 calc_wt_size() {
@@ -405,7 +406,7 @@ do_deinstall() {
 #
 calc_wt_size
 while true; do
-  FUN=$(whiptail --title "Server Software Configuration Tool Bigwolf2000 $vers" --menu "Setup Options" $WT_HEIGHT $WT_WIDTH $WT_MENU_HEIGHT --cancel-button Beenden --ok-button Wählen \
+  FUN=$(whiptail --title "Server Software Configuration Tool Bigwolf2000 version" --menu "Setup Options" $WT_HEIGHT $WT_WIDTH $WT_MENU_HEIGHT --cancel-button Beenden --ok-button Wählen \
     "1 Docker                    " "Löschen, usw." \
 	"2 Change User Password      " "Root Password ändern" \
     "3 Grund-optionen            " "Sprache-Zeit-Tastatur Tasksel Backup " \
