40f267c30cc914d3340f606d93c4be43996e90b8
Wolf2000 <support@wolf2000.at>
2022-09-11 00:29:35 +0200

    Update omv-install-6.x.sh

--- omv-install-6.x.sh
+++ omv-install-6.x.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-#
+
 apt-get install --yes gnupg &&
 wget -O "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.asc" https://packages.openmediavault.org/public/archive.key &&
 apt-key add "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.asc" &&
