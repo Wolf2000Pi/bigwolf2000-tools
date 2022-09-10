e0c4369b23fe0152ca03ec205f627cdbc3849a08
Wolf2000 <support@wolf2000.at>
2022-09-10 22:11:35 +0200

    Update backupdocker.sh

--- backupdocker.sh
+++ backupdocker.sh
@@ -4,7 +4,7 @@ DATE=$(date +%d-%m-%Y-%H_%M_%S)
 BACKUP_DIR="/srv/backup/docker"
 SOURCE="/srv/docker/"
 
-find $BACKUP_DIR -name '*' -mtime +20 -exec sudo rm -rf {} \;
+find $BACKUP_DIR -name '*' -mtime +10 -exec sudo rm -rf {} \;
 #find $BACKUP_DIR -name '*.tar.gz' -mtime +30 -exec sudo rm -rf {} \;
 
 cd /
