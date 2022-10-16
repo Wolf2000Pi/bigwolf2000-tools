4a5f6dab18980df515015992bb96044ebd38863e
Wolf2000 <support@wolf2000.at>
2022-10-16 16:12:58 +0200

    secret.session.strin
    secret.session.strin

--- offi.sh
+++ offi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
-docker exec $(docker ps -l -q --filter "name=srv-captain--offi.1") /var/www/onlyoffice/documentserver/npm/json -f /etc/onlyoffice/documentserver/local.json 'services.CoAuthoring.secret.session.string' &&
+#docker exec $(docker ps -l -q --filter "name=srv-captain--offi.1") /var/www/onlyoffice/documentserver/npm/json -f /etc/onlyoffice/documentserver/local.json 'services.CoAuthoring.secret.session.string' &&
 docker exec $(docker ps -l -q --filter "name=srv-captain--offi.1") supervisorctl start ds:example &&
 docker exec $(docker ps -l -q --filter "name=srv-captain--offi.1") sed 's,autostart=false,autostart=true,' -i /etc/supervisor/conf.d/ds-example.conf
